83fa4e68f9c1e83a85c34c439b3785954abbc03a
pjay79 <praveen.jayarajan@gmail.com>
2019-03-24 13:00:53 +1100

    Update maps

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -34,8 +18,8 @@ export default class App extends Component<Props> {
         >
           <Marker
             coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-            title="title"
-            description="description"
+            title="Location"
+            description="Starbucks"
           />
         </MapView>
       </View>
