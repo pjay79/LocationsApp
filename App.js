ebe303e540620541598142854309439d21eed00f
pjay79 <praveen.jayarajan@gmail.com>
2019-03-24 13:12:49 +1100

    Add eslint

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          showsUserLocation={true}
-        >
-          <Marker
-            coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-            title="Location"
-            description="Starbucks"
-          />
-        </MapView>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      showsUserLocation
+    >
+      <Marker
+        coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+        title="Location"
+        description="Starbucks"
+      />
+    </MapView>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -38,3 +34,5 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+
+export default App;
